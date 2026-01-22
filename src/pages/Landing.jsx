76195110249d86fa904ad72dcfde73a69f7eb96c
krishna <krishna613460@gmail.com>
2026-01-22 21:27:24 +0545

    Added landing page for loading hero or services component

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import { Hero, Services, Process, CTA } from '../components/sections';
+
+export default function Landing() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Process />
+      <CTA />
+    </>
+  );
+}
